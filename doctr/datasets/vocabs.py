df31b54bec2e93a9eddf05b2cc1b62ca4b8d2d33
Azamat Sultanov <azamat.sultonov.98@gmail.com>
2025-03-16 02:01:15 +0500

    Added missing chars

--- doctr/datasets/vocabs.py
+++ doctr/datasets/vocabs.py
@@ -75,7 +75,7 @@ VOCABS["ukrainian"] = (
 )
 
 VOCABS["tajik"] = (
-    VOCABS["generic_cyrillic_letters"] + VOCABS["digits"] + VOCABS["punctuation"] + "ҒғҚқҲҳҶҷӢӣӮӯ"
+    VOCABS["generic_cyrillic_letters"] + VOCABS["digits"] + VOCABS["punctuation"] + "ёЁъЪҒғҚқҲҳҶҷӢӣӮӯ"
 )
 
 VOCABS["multilingual"] = "".join(
